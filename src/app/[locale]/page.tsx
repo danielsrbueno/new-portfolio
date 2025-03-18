b59c9d5aa7a0758ae31a3412b9675788aa7b0b2e
Daniel da Silva Ramos Bueno <jj.danielbr10@gmail.com>
2025-03-18 17:44:27 -0300

    feat: add navbar

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,63 +1,9 @@
-"use client"
-
 import Navbar from "@/components/shared/Navbar"
-import gsap from "gsap"
-import { useTranslations } from "next-intl"
-import { useLayoutEffect, useRef } from "react"
 
 export default function Home() {
-  const t = useTranslations()
-  const hour = new Date().getHours()
-  const comp = useRef(null)
-  let greeting
-
-  if (hour >= 6 && hour < 12) greeting = t("greeting.morning")
-  else if (hour >= 12 && hour < 18) greeting = t("greeting.afternoon")
-  else greeting = t("greeting.night")
-
-  useLayoutEffect(() => {
-    const introSlider = document.querySelector("#intro-slider")
-    const title1 = document.querySelector("#title-1")
-    const title2 = document.querySelector("#title-2")
-
-    const ctx = gsap.context(() => {
-      const t1 = gsap.timeline()
-      t1.from(title1, {
-          opacity: 0,
-          y: "+=35",
-          stagger: 0.5,
-      }).from(title2, {
-          opacity: 0,
-          y: "+=35",
-          stagger: 0.5,
-          delay: 1,
-      }).to(title1, {
-          opacity: 0,
-          y: "-=30",
-          delay: 1, 
-          stagger: 0.5,
-      }).to(title2, {
-          opacity: 0,
-          y: "-=30",
-          delay: 0,
-          stagger: 0.5,
-      })
-      .to(introSlider, {
-          xPercent: "-100",
-          duration: 1.3,
-      })
-    }, comp)
-
-    return () => ctx.revert()
-  }, [])
-
   return (
     <>
-      <div id="window" className="w-screen h-screen overflow-x-hidden bg-zinc-100 selection:bg-zinc-800 selection:text-zinc-50 dark:bg-slate-900 dark:selection:bg-zinc-300 dark:selection:text-zinc-900 transition-all scrollbar scrollbar-thin scrollbar-track-zinc-100 scrollbar-thumb-zinc-400 scroll-thumb-rounded-full dark:scrollbar-track-zinc-900 dark:scrollbar-thumb-zinc-600 ">
-        <div className='h-screen p-10 bg-zinc-900 absolute top-0 left-0 z-50 w-full flex flex-col gap-4 tracking-tighter font-black font-jetbrains items-center justify-center  text-zinc-200' id='intro-slider'>
-          <h1 className='max-sm:text-2xl sm:text-5xl lg:text-6xl' id='title-1'>{greeting}</h1>
-          <h1 className='max-sm:text-2xl sm:text-5xl lg:text-6xl' id='title-2'>{t("welcome")}</h1>
-        </div>
+      <div className="w-screen h-screen overflow-x-hidden bg-zinc-100 selection:bg-zinc-800 selection:text-zinc-50 dark:bg-zinc-900 dark:selection:bg-zinc-300 dark:selection:text-zinc-900 transition-all scrollbar scrollbar-thin scrollbar-track-zinc-100 scrollbar-thumb-zinc-400 scroll-thumb-rounded-full dark:scrollbar-track-zinc-900 dark:scrollbar-thumb-zinc-600 ">
         <Navbar />
       </div>
     </>
--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -1,146 +1,73 @@
 "use client"
 
-import { gsap } from "gsap"
-import { ScrollTrigger } from "gsap/ScrollTrigger"
-import { ScrollToPlugin } from "gsap/all"
-import { useEffect, useState, useLayoutEffect } from "react"
+import { BriefcaseBusiness, ContactRound, House, PhoneCall, Settings2 } from "lucide-react"
 import { useTranslations } from "next-intl"
 import { JetBrains_Mono } from "next/font/google"
+import Link from "next/link"
+import { useEffect, useState } from "react"
 
 const jetBrainsMono = JetBrains_Mono({
   subsets: ["latin"]
 })
 
 export default function Navbar() {
-  gsap.registerPlugin(ScrollTrigger)
-  gsap.registerPlugin(ScrollToPlugin)
-
-  const [menu, setMenu] = useState(false)
-  const [isMobile, setIsMobile] = useState(false)
-
   const t = useTranslations("nav")
 
-  const handleResize = () => {
-    if (window.innerWidth <= 980) setIsMobile(true)
-    else setIsMobile(false)
-  }
+  const liStyle = "cursor-pointer hover:text-zinc-950 transition-all animate-ease border-b-transparent border-b-2 hover:border-zinc-800/95 active:text-zinc-950 active:border-zinc-800/95 dark:hover:border-zinc-100/95 dark:hover:text-zinc-100"
+
+  const [isMobile, setIsMobile] = useState<boolean>(window.innerWidth <= 768)
 
   useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768)
+    }
+
     handleResize()
     window.addEventListener("resize", handleResize)
+
     return () => window.removeEventListener("resize", handleResize)
   }, [])
 
-  const [section, setSection] = useState("")
-
-  useEffect(() => {
-    if (section) {
-      gsap.to("#window", {
-        duration: 0.75,
-        ease: "expo.out",
-        scrollTo: { y: section, offsetY: 80 }
-      })
-    }
-  }, [section])
-
-  useLayoutEffect(() => {
-    const btn = document.getElementById("btn-menu")
-    const nav = document.querySelector("nav")
-    const ul = document.querySelector("ul")
-
-    if (isMobile && menu) {
-      gsap.to(nav, {
-        duration: 0.5,
-        height: "100vh",
-        ease: "expo.out",
-      });
-      gsap.to(ul, {
-        duration: 0.5,
-        opacity: 1,
-        ease: "expo.out",
-      });
-      gsap.to(btn, {
-        duration: 0.5,
-        rotate: 180,
-        ease: "expo.out",
-      });
-    } else if (isMobile && !menu) {
-      gsap.to(nav, {
-        duration: 0.5,
-        height: "4.5rem",
-        ease: "expo.out",
-      });
-      gsap.to(ul, {
-        duration: 0.5,
-        opacity: 0,
-        ease: "expo.out",
-      });
-      gsap.to(btn, {
-        duration: 0.5,
-        rotate: 0,
-        ease: "expo.out",
-      });
-    }
-  }, [isMobile, menu]);
-
-  const fnUl = () => {
-    if (isMobile && !menu) return " hidden "
-    else if (isMobile && menu)
-      return " flex flex-col gap-7 text-zinc-700 font-semibold text-lg text-center"
-    else if (!isMobile)
-      return " font-semibold text-lg flex gap-7 text-zinc-700 "
-  }
-
-  const fnNav = () => {
-    if (isMobile && !menu)
-      return " justify-between w-screen fixed backdrop-blur-sm flex items-center select-none z-40 min-sm:px-4 max-sm:px-12 max-md:px-20 max-lg:px-24 h-20 "
-    else if (isMobile && menu)
-      return " w-screen h-screen absolute top-0 left-0 select-none flex flex-col items-center justify-center backdrop-blur-xl z-40 gap-7 text-zinc-950 font-semibold text-lg"
-    else if (!isMobile)
-      return " justify-around w-screen fixed backdrop-blur-sm flex items-center select-none z-40 min-sm:px-4 max-sm:px-12 max-md:px-20 max-lg:px-24 h-20"
-  }
-
   return (
-    <nav className={fnNav() + " dark:text-zinc-300 dark:bg-zinc-900/20 bg-zinc-100/10"}>
+    <nav className="w-7/12 h-24 left-12 top-1 px-12 rounded-full rounded-tl-none fixed flex items-center justify-between backdrop-blur-sm select-none z-40 dark:text-zinc-300 dark:bg-zinc-900/20 max-xl:w-9/12 max-lg:px-10 max-md:w-10/12 max-md:h-20 max-md:top-auto max-md:bottom-5 max-md:left-0 max-md:right-0 max-md:mx-auto max-md:rounded-full max-sm:h-16">
       <h1
-      className={ (isMobile && menu ? " text-4xl  font-extrabold my-gradient-radial -mt-12 mb-10" : " font-extrabold my-gradient-radial max-sm:text-2xl max-lg:text-4xl lg:text-5xl ") + `${jetBrainsMono.className}` }>
-        &lt;Daniel /&gt;
+      className={`${jetBrainsMono.className} font-extrabold my-gradient-radial max-sm:text-2xl max-lg:text-3xl lg:text-5xl max-md:hidden`}>
+        &lt;Dan/&gt;
       </h1>
-
-      <ul className={fnUl() + " dark:text-zinc-300"}>
-        <li
-        id="link-1"
-        className="cursor-pointer hover:text-zinc-950 transition-all animate-ease border-b-transparent border-b-2 hover:border-zinc-800/95 active:text-zinc-950 active:border-zinc-800/95 dark:hover:border-zinc-100/95 dark:hover:text-zinc-100"
-        onClick={() => { setSection("#introduction"); setMenu(false); }}>
-          {t("home")}
-        </li>
-        <li
-          className="cursor-pointer hover:text-zinc-950 transition-all animate-ease border-b-transparent border-b-2 hover:border-zinc-800/95 active:text-zinc-950 active:border-zinc-800/95 dark:hover:border-zinc-100/95 dark:hover:text-zinc-100"
-          onClick={() => {
-            setSection("#about");
-            setMenu(false);
-          }}
-        >
-          {t("about")}
-        </li>
-        <li
-          className="cursor-pointer hover:text-zinc-950 transition-all animate-ease border-b-transparent border-b-2 hover:border-zinc-800/95 active:text-zinc-950 active:border-zinc-800/95 dark:hover:border-zinc-100/95 dark:hover:text-zinc-100 "
-          onClick={() => {
-            setSection("#projects");
-            setMenu(false);
-          }}
-        >
-          {t("projects")}
-        </li>
-        <li className="cursor-pointer hover:text-zinc-950 transition-all animate-ease border-b-transparent border-b-2 hover:border-zinc-800/95 active:text-zinc-950 active:border-zinc-800/95 dark:hover:border-zinc-100/95 dark:hover:text-zinc-100" onClick={() => { setSection("#contact"); setMenu(false) }}> {t("contact")} </li>
-      </ul>
-      <button 
-      id="btn-menu" 
-      className={
-        (isMobile ? "absolute top-7 right-10 pi pi-bars text-2xl z-50 " : "hidden ") +
-        (isMobile && menu ? "absolute top-7 right-10 text-2xl" : "") + " select-none"
+      {!isMobile && 
+        <ul className="flex font-medium text-lg mr-[10%] gap-7 text-zinc-700 dark:text-zinc-300 max-lg:font-semibold max-lg:text-base max-lg:gap-4 max-lg:mr-2">
+          <Link href="/">
+            <li className={liStyle}>{t("home")}</li>
+          </Link>
+          <Link href="/about">
+            <li className={liStyle}>{t("about")}</li>
+          </Link>
+          <Link href="/projects">
+            <li className={liStyle}>{t("projects")}</li>
+          </Link>
+          <Link href="/contact">
+            <li className={liStyle}>{t("contact")}</li>
+          </Link>
+        </ul>
       }
-      onClick={() => setMenu(!menu)}/>
+      {isMobile &&
+        <ul className="w-full flex gap-10 items-center justify-center text-zinc-700 dark:text-zinc-300">
+          <Link href="/about">
+            <ContactRound className="w-8 h-8 cursor-pointer hover:scale-110 hover:text-zinc-950 dark:hover:text-zinc-50" />
+          </Link>
+          <Link href="/projects">
+            <BriefcaseBusiness className="w-8 h-8 cursor-pointer hover:scale-110 hover:text-zinc-950 dark:hover:text-zinc-50" />
+          </Link>
+          <Link href="/">
+            <House className="w-8 h-8 cursor-pointer hover:scale-110 hover:text-zinc-950 dark:hover:text-zinc-50" />
+          </Link>
+          <Link href="/contact">
+            <PhoneCall className="w-7 h-7 cursor-pointer hover:scale-110 hover:text-zinc-950 dark:hover:text-zinc-50" />
+          </Link>
+          <Settings2 className="w-8 h-8 cursor-pointer hover:scale-110 hover:text-zinc-950 dark:hover:text-zinc-50" />
+          {/* <Notebook className="w-8 h-8" /> */}
+        </ul>
+      }
     </nav>
   )
 }
