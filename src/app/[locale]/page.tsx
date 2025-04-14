52e0a31adcee9b81f07c7e4e092bc0fc00ce8a0b
Daniel da Silva Ramos Bueno <jj.danielbr10@gmail.com>
2025-04-14 16:17:17 -0300

    feat: add about and projects section

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,12 +1,16 @@
-import Introduction from "@/components/shared/Introduction"
+import About from "@/components/shared/About"
+import Introduction from "@/components/shared/Introduction" 
 import Navbar from "@/components/shared/Navbar"
+import Projects from "@/components/shared/Projects"
 
 export default function Home() {
   return (
     <>
-      <div className="w-screen h-screen overflow-x-hidden transition-all bg-zinc-100 dark:bg-zinc-950 dark:selection:bg-zinc-300 dark:selection:text-zinc-900 selection:bg-zinc-800 selection:text-zinc-50 scrollbar scrollbar-thin scrollbar-track-zinc-100 scrollbar-thumb-zinc-400 scroll-thumb-rounded-full dark:scrollbar-track-zinc-900 dark:scrollbar-thumb-zinc-600 ">
+      <div id="main" className="w-screen h-screen overflow-x-hidden transition-all bg-background text-foreground selection:bg-zinc-800 selection:text-zinc-50 dark:selection:bg-zinc-300 dark:selection:text-zinc-900 scrollbar scrollbar-thin scrollbar-track-zinc-100 scrollbar-thumb-zinc-400 scroll-thumb-rounded-full dark:scrollbar-track-zinc-900 dark:scrollbar-thumb-zinc-600 scroll-smooth">
         <Navbar />
         <Introduction />
+        <About />
+        <Projects />
       </div>
     </>
   );
